ab7b91cd7d25a578e565f8c23c6903e025478b55
Fabio Soares <fssoares3@useniu.com.br>
2025-11-18 22:05:31 -0300

    feat(fat_comportamento_inicial): Criação da fato

--- models/birdbase_gold/fat_comportamentos_sociais.sql
+++ models/birdbase_gold/fat_comportamentos_sociais.sql
@@ -15,14 +15,13 @@ SELECT
     fat_aves.nm_cientifico  AS nm_cientifico,
     CONCAT(dim_ordens.tp_ordem_latin, ' (',dim_ordens.ds_ordem,')') AS tp_ds_ordem,
     {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_colonial) AS tp_comportamento_colonial,
-    {{target.schema}}.tratarCampoBoolString(fat_aves.is_comportamento_forma_bando) AS is_comportamento_forma_bando,
+    {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_forma_bando) AS is_comportamento_forma_bando,
     {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_pares_grupos_familiares) AS is_comportamento_pares_grupos_familiares,
     {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_solitario_pares) AS tp_comportamento_solitario_pares,
     {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_solitario) AS tp_comportamento_solitario,
-    -- {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_nomadico) AS tp_comportamento_nomadico,
-    -- {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_sedentario) AS tp_comportamento_sedentario,
-    -- {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_territorial) AS tp_comportamento_territorial,
-    -- {{ target.schema }}.tratarCampoBoolString(fat_aves.is_comportamento_parasitismo_nidal) AS tp_comportamento_parasitismo_nidal
+    {{ target.schema }}.tratarCampoBoolString(fat_aves.is_monogamia) AS tp_comportamento_monogamia,
+    {{ target.schema }}.tratarCampoBoolString(fat_aves.is_poligamia) AS tp_comportamento_poligamia,
+    {{ target.schema }}.tratarCampoBoolString(fat_aves.is_criacao_cooperativa) AS tp_comportamento_criacao_cooperativa,
 FROM
   {{ ref('fat_aves') }} fat_aves
 LEFT JOIN {{ ref('dim_ordens') }} dim_ordens
